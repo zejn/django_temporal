e91c652400111bd20773ade0f90620273b3a7789
Gasper Zejn <zejn@kiberpipa.org>
2013-08-14 17:39:46 +0200

    Add setup script.

---
+++ setup.py
@@ -0,0 +1,18 @@
+
+from setuptools import setup, find_packages
+
+setup(name='django_temporal',
+      version='1.0',
+      description='Temporal database extension for Django',
+      author='Gasper Zejn',
+      author_email='zejn@kiberpipa.org',
+      url='http://github.com/zejn/django_temporal',
+      packages=find_packages(exclude=['temporal']),
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Intended Audience :: Developers',
+          'Topic :: Database',
+          'Framework :: Django',
+          'License :: OSI Approved :: BSD License'
+        ]
+     )

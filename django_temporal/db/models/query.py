ce5bbfd1ea3050f6a4a63a7dd9a987c91c9df27e
Gasper Zejn <zejn@kiberpipa.org>
2014-10-24 21:07:51 +0200

    Future compatibility.

--- django_temporal/db/models/query.py
+++ django_temporal/db/models/query.py
@@ -2,6 +2,6 @@ from django.db.models.query import QuerySet, Q, ValuesQuerySet, ValuesListQueryS
 from django_temporal.db.models.sql.query import TemporalQuery
 
 class TemporalQuerySet(QuerySet):
-    def __init__(self, model=None, query=None, using=None):
-        super(TemporalQuerySet, self).__init__(model=model, query=query, using=using)
+    def __init__(self, model=None, query=None, using=None, **kwargs):
+        super(TemporalQuerySet, self).__init__(model=model, query=query, using=using, **kwargs)
         self.query = query or TemporalQuery(self.model)
